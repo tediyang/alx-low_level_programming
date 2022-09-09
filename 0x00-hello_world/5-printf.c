64b912db6f0dd6fa1bc15505f1f10993b99a7216
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-09-08 17:20:42 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the message below
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	
+	return (0);
+
+}
+
