41d439c0f2948e1ad4f426d159d93175468eb166
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-09-09 18:52:27 -0700

    initial commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 
-/* 
+/** 
  * main - this prints if the result of n if it value is negative positive or neutral.
  *
  * Return: this always return 0 because the function is a int function.
@@ -15,12 +15,12 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
+	if (n>0)
+		printf("%d is positive\n",n);
+	else if (n==0)
+		printf("%d is zero\n",n);
 	else
-		printf("%d is negative\n", n);
+		printf("%d is negative\n",n);
 
 	return (0);
 }
