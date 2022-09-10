5c0afd1f1767a2074f56c7489e0f24d9af734121
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-09-10 10:02:54 -0700

    4-print

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -prints alphabet except q and e
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a != 'q' || a != 'e')
+		{
+			putchar(a);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
