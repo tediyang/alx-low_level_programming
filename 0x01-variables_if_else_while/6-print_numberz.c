39fc3dbec9330d779cfe7cf04172872b553a9003
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-09-10 12:16:22 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+
+	return (0);
+}
