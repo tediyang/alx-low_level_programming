85f43be3bafc7995e948583a494d6eeac95c8542
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-09-10 11:18:26 -0700

    print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - this prints number
+ *
+ * Return: this returns 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d\n", a);
+	}
+	
+	return (0);
+}
