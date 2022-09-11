ddf8f9233cd73825efc71d8fab1e19ecf7e37983
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-09-11 08:43:00 -0700

    combo 3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - possible two digits
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int a, b, c, d, e;
+
+	for (a = 0; a <= 8; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			e = b - 1;
+			if (a != b && e < 0 && a > e)
+			{
+				c = a + '0';
+				d = b + '0';
+				putchar(c);
+				putchar(d);
+			}
+			if (a != 8 && b != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
