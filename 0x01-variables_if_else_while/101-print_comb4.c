51845a84cddd9aad2696429ddf2c3c06b022dc64
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-09-12 09:16:24 -0700

    combo

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,30 +8,24 @@
 
 int main(void)
 {
-	int a, b, c, d, e, f;
+	int a, b, c;
 
 	for (a = 0; a <= 7; a++)
 	{
 		for (b = 0; b <= 8; b++)
 		{
-			if (a != b && a <= (b - 1) && (b - 1) >= 0)
+			for (c = 0; c <= 9; c++)
 			{
-				for (c = 0; c <= 9; c++)
+				if (c > b && b > 1)
 				{
-					if (b != c && b <= (c - 1) && (c - 1) >= 0)
-					{
-						d = a + '0';
-						e = b + '0';
-						f = c + '0';
-						putchar(d);
-						putchar(e);
-						putchar(f);
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(c + '0');
 
-						if (!(a == 7 && b == 8 && c == 9))
-						{
-							putchar(',');
-							putchar(' ');
-						}
+					if (!(a == 7 && b == 8 && c == 9))
+					{
+						putchar(',');
+						putchar(' ');
 					}
 				}
 			}
