984a5f76062ef42bf56e14143f45f1f0de2e8aa6
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-09-14 15:36:13 -0700

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,24 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - check the code
- *
  * print_alphabet - prints the alphabet.
  *
- * Return: returns 0
+ * Return: returns void
  */
-
-int main(void)
-{
-	void print_alphabet(void);
-	return (0);
-}
 
 void print_alphabet(void)
 {
 	char a;
-	for (a = 'a'; a <= 'z'; a++)
-		putchar(a);
 
-	putchar('\n');
+	for (a = 'a'; a <= 'z'; a++)
+		_putchar(a);
+
+	_putchar('\n');
+
+	return;
 }
