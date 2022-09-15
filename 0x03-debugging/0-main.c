0190caf33e37161feb94b1a4a615e08de8b2caf5
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-09-15 11:40:52 -0700

    initial files

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - checks the code
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	positive_or_negative(a);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This is debugging on alx software engineering course
