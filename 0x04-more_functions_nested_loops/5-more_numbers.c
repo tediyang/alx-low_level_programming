8261d88b8cc70c1145a9a3eaeb84c98945b2f365
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-09-16 14:51:16 -0700

    more number

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints more
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int c, d;
+
+	for (c = 0; c < 10; c++)
+		for (d = 0; d <= 14; d++)
+			_putchar(d + '0');
+
+	_putchar('\n');
+}
