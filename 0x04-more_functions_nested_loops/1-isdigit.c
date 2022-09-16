b42dd4977b431389b459c59be903c71874780495
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-09-16 12:21:27 -0700

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for digits
+ * @c: parameter
+ * Return: returns 0
+ */
+
+int isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
