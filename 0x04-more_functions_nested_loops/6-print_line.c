53013a2f96048e418ef6837820b6fef6bb11af24
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-09-16 16:24:16 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/** print_line - prints line
+ * @n: parameter
+ * return: void
+ */
+
+void print_line(int n)
+{
+	int a;
+
+	for (a = 0; a < n; a++)
+		_putchar('_');
+
+	_putchar('\n');
+}
