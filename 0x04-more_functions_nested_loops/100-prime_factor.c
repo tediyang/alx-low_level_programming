f995082bf9c8e11a72914646ebe9825319ea1f27
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-09-16 18:45:51 -0700

    prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print prime
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long int a = 3, n = 612852475143;
+
+	for (; a < 12057; a +=2)
+	{
+		while (n % a == 0 && n != a)
+			n /= a;
+	}
+	printf("%lu\n", n);
+	return (0);
+}
