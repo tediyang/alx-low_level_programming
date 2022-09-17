47ee5b2b4699932cb0f518d9bbfc91fdbb0a7cb9
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-09-16 17:15:28 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,10 +10,17 @@ void print_line(int n)
 {
 	int a = 1;
 
-	while (a <= n)
+	if (n <= 0)
 	{
-		_putchar(95);
-		a++;
+		while (a <= n)
+		{
+			_putchar(95);
+			a++;
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		_putchar('\n');
+	}
 }
