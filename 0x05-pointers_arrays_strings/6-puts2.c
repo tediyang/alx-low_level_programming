20c99e4fafff3248ed0744f913c0a6e7f58b848e
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-09-20 23:37:25 -0700

    puts 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts2 - prints a string
+ * @str: param
+ */
+
+void puts2(char *str)
+{
+	int a, len;
+
+	len = strlen(str);
+
+	for (a = 0; a < len; a = a + 2)
+	{
+		putchar(str[a]);
+	}
+	putchar('\n');
+}	
+
