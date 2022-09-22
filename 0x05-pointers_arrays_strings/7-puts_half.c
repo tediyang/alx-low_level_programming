3ad8d6e285c0de3783347af7a723fd4e3a5b4d80
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-09-22 03:49:07 -0700

    puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * puts_half - prints half of string
+ * @str: param
+ */
+
+void puts_half(char *str)
+{
+	int a, len;
+
+	len = strlen(str);
+
+	for (a = len / 2; a <= len; a++)
+	{
+		putchar(str[a]);
+	}
+	putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,5 +8,6 @@ void _puts(char *s);
 void print_rev(char *c);
 void rev_string(char *s);
 void puts2(char *str);
+void puts_half(char *str);
 
 #endif
