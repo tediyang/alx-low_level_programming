c7d5006d87f662249a4cff594f46b485f1a39dbf
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-09-22 05:30:11 -0700

    strings concat 1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - concat string
+ * @dest: param
+ * @src: param
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a, b;
+
+	a = 0;
+	/*find the length of the dest file*/
+	while (dest[a])
+		a++;
+
+	for (b = 0; src[b]; b++)
+	{
+		dest[a++] = src[b];
+	}
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is a project on C pointers, arrays and strings.
