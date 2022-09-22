e77eb42b07b4a66259f3acaf80a1b8f4b4f76c01
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-09-22 07:13:09 -0700

    string concat with int

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strncat - concat n strings
+ * @dest: point param
+ * @src: point param
+ * @n: int param
+ * Return: dest 
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int a, b, c;
+
+	a = 0;
+	b = 0;
+	/*get the lengths of dest and src*/
+	while (dest[a])
+		a++;
+	while (dest[b])
+		b++;
+
+	if ((b - 1) < n)
+	{
+		for (c = 0; c < b; c++)
+		{
+			dest[a] = src[c];
+			a++;
+		}
+	}
+	else
+	{
+		for (c = 0; c < n; c++)
+		{
+			dest[a] = src[c];
+			a++;
+		}
+	}
+	return (dest);
+}
