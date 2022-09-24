614b16d5563e729d67b579e098b3885f724e12c1
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-09-23 18:04:22 -0700

    cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,9 +11,13 @@ char *cap_string(char *s)
 	int a = 0;
 
 	while (s[a] != '\0')
+	{
+		if (a == 0 && s[a] >= 97 && s[b] <= 122)
+			s[a] = s[a] - 32;
 		if (s[a] == ' ')
 			if (s[a + 1] != '\0' && s[a + 1] >= 97 && s[a + 1] <= 122)
 				s[a + 1] = s[a + 1] - 32;
+	}
 
 	return (s);
 }
