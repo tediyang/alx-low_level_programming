e5bdfaf6a2e433bdd79134bc3b30b3ceced53dd0
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-09-27 14:18:04 -0700

    memory set correction

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,19 +2,21 @@
 
 /**
  * _memset - fills memory with a constant byte
- * @s: param
- * @b: param
- * @n: param
+ * @s: point param
+ * @b: char param
+ * @n: int param
  *
  * Return: returns pointer
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	for (; i < n; i++)
+	for (i = 0; i < n; i++)
+	{
 		s[i] = b;
+	}
 
 	return (s);
 }
