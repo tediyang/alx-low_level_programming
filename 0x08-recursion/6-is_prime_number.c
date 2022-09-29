712286553edbf5e3c72b0fde7be1ffa868ea41f2
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-09-28 20:24:04 -0700

    prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * is_prime_number - prints prime number.
+ *
+ * @n: param
+ * @i: divisor
+ *
+ * Return: the value
+ */
+
+int prime_number(int n, int i);
+
+int _is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	else
+		return (prime_number(n, 2));
+}
+
+/**
+ * prime_number - return prime
+ *
+ * @n: number
+ * @i: divisor
+ *
+ * Return: the value
+ */
+
+int prime_number(int n, int i)
+{
+	if (i <= n && (n % i) == 0 && i != n)
+		return (0);
+	else if (i == n && (n % i) == 0)
+		return (1);
+	else
+		return (prime_number(n, i + 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,5 +9,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int check_num(int n, int b);
 int _sqrt_recursion(int n);
+int prime_number(int n, int i);
+int is_prime_number(int n);
 
 #endif
