c2571a19d5a90ac50334b0eacf01ae1712474e4b
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-09-30 13:01:53 -0700

    multiply

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,14 +11,14 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc > 2)
-	{
-		printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
-	}
-	else
+	if (argc < 3)
 	{
 		printf("Error\n");
 		return (1);
+	}
+	else
+	{
+		printf("%d\n", (atoi(agrv[agrc - 1]) * atoi(argv[argc - 2]));
 	}
 	return (0);
 }
