a383d544db4a5d3ce12773a4bc42a001d3faaf19
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-09-30 11:11:53 -0700

    initial commit and first task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the program name
+ *
+ * @argc: param
+ * @argv: param
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+How to use the arguments of main in C
