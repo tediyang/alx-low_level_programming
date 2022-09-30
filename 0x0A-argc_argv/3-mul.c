6ea5bda40ea8d54e58ceb0595ba2add1c2e1a39e
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-09-30 12:43:43 -0700

    multiply

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,14 +6,20 @@
  * @argc: command line args count
  * @argv: command line args array
  *
- * Return: 0
+ * Return: 0 or 1
  */
 
 int main(int argc, char *argv[])
 {
-	if (argv[argc - 2] && argv[argc -1])
-		printf("%d\n", (atoi(argv[argc - 2]) * atoi(argv[argc -1])));
+	(void) argc;
+	if (argv[1] && argv[2])
+	{
+		printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
+		return (0);
+	}
 	else
-		printf("Error");
-	return (0);
+	{
+		printf("Error\n");
+		return (1);
+	}
 }
