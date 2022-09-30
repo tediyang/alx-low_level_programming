cc8096de43f0568d177ecc4008a5c4bd2471ffcd
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-09-30 12:01:41 -0700

    args count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of args
+ *
+ * @argc: command line args count
+ * @argv: command line args array
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv[];
+	printf("%d\n", argc);
+}
