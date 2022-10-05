8cd3d701463529d64f5c748a92f8ed984b26424f
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-10-05 14:43:03 -0700

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 	if (size <= 0)
 		return ('\0');
 
-	array = (char *)malloc(sizeof(char) * size);
+	array = (char*)malloc(sizeof(char) * size);
 
 
 	for (i = 0; i < size; i++)
