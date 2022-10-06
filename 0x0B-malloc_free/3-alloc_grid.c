d4f6b7f7cf179804ad35748fa9d2bf3993ff12a5
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-10-05 17:32:51 -0700

    grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - return pointer to two dimensional array
+ *
+ * @width: width param
+ * @height: height param
+ *
+ * Return: pointer
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid, a, b;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = (int *)malloc(sizeof(int *) * height);
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (a = 0; a < height; a++)
+	{
+		grid[a] = (int)malloc(sizeof(int) * width);
+		if (grid[a] == NULL)
+		{
+			while (a >= 0)
+			{
+				free(grid[a]);
+				a--;
+			}
+			free(grid);
+			return (NULL);
+		}
+		for (b = 0; b < width; b++)
+			grid[a][b] = 0;
+	}
+	return (grid);
+}
