da0e3009b69d486eed6756f1a0fe9eb68ee72ce7
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-10-06 09:31:10 -0700

    malloc check

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - check if a memory pointer was created.
+ *
+ * @b: param
+ *
+ * Return: nothing
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *mem;
+
+	mem = malloc(b * sizeof *mem);
+
+	if (mem == NULL)
+		exit(98);
+
+	return (mem);
+}
