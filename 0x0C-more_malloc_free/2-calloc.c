8b359cd4582d7c5c6bb9e75edd5c3906dd33f79c
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-10-06 18:16:36 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 
 /**
+ * _memset - set the memory to zero just like calloc does
+ *
+ * @mem: memory set
+ * @a: int param which is zero
+ * @space: nmemb and size
+ *
+ * Return: char pointer
+ */
+
+char *_memset(char *mem, unsigned int a, unsigned int space)
+{
+	unsigned int i;
+
+	for (i = 0; i < space; i++)
+		mem[i] = a;
+	return (mem);
+}
+
+/**
  * _calloc - create a mem and return a pointer
  *
  * @nmemb: param
@@ -12,7 +31,7 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *mem;
+	char *mem;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -21,6 +40,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (mem == NULL)
 		return (NULL);
+
+	_memset(mem, 0, size * nmemb);
 
 	return (mem);
 }
