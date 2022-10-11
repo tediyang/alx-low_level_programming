62dc76cf46a5f15f3785b2b854d8bf116eb341c8
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-10-11 05:15:56 -0700

    absolute

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
+#ifndef ABS_H
+#define ABS_H
 
-#define ABS(x) (((x) < 0)? -1 * (x) : (x))
+#define ABS(x) (((x) < 0) ? -1 * (x) : (x))
 
 #endif /* ifndef */
