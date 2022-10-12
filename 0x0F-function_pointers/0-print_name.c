dae5635fdb2b1129bc28628aa2c9a87682194c32
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-10-12 14:05:37 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print the name.
+ *
+ * @name: string param
+ * @f: function param
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
