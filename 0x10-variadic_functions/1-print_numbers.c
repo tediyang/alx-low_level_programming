637d235f9bc5ac255ce72de70c54e5b637ed2a87
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-10-14 15:03:13 -0700

    print number

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * print_numbers - prints the number
+ *
+ * @separator: param
+ * @n: total number of variable
+ */
+
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	if (separator == NULL)
