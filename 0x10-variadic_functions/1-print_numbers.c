980af52497852dc8e22634a4a06057d19e5960a8
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-10-14 15:49:39 -0700

    print number

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,19 +10,19 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+
 	if (separator)
 	{
-		unsigned int i;
-		va_list args;
-
-		va_start(args, n);
-
 		for (i = 0; i < n; i++)
 		{
 			if (i == 0)
 				printf("%d", va_arg(args, int));
 			else
-				printf("%s %d", separator, va_arg(args, int));
+				printf("%c %d", *separator, va_arg(args, int));
 		}
 
 		va_end(args);
