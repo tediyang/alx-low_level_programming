f15d7e66b8e30f7a69af70f8aee18ac184ed4219
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-10-14 13:35:59 -0700

    sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,14 +13,14 @@ int sum_them_all(unsigned int n, ...)
 	unsigned int i, sum = 0;
 	va_list ags;
 
-	va_start(args, n);	
+	va_start(ags, n);	
 
 	if (n == 0)
 		return (0);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(args, unsigned int);
+		sum += va_arg(ags, unsigned int);
 
-	va_end(args);
+	va_end(ags);
 	return (sum);
 }
