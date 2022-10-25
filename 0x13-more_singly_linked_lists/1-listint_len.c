2b5bd7867f0ac857e4c7fceb6b26d4c6c64d4c40
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-10-25 14:43:21 -0700

    element

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -14,16 +14,17 @@ size_t listint_len(const listint_t *h)
 	int count = 0;
 
 	if (h == NULL)
-		return (0);
+		return (0);i
 
 	while (h != NULL)
 	{
+		if (h->n != '\0')
+			count++;
+
 		if (h->next == NULL)
 			h = NULL;
 		else
-			h = h->next;
-
-		count++;
+			h = h->next
 	}
 
 	return (count);
