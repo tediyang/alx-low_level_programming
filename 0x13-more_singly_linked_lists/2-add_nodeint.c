570bf6a21fabe111a968d62982e9d097417967cb
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-10-26 06:47:11 -0700

    add node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -14,16 +14,15 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *first;
 
 	first = malloc(sizeof(listint_t));
-
 	if (first == NULL)
 		return (NULL);
-
-	first->n = n;
 
 	if (*head == NULL)
 		first->next = NULL;
 	else
 		first->next = *head;
+
+	first->n = n;
 
 	*head = first;
 
