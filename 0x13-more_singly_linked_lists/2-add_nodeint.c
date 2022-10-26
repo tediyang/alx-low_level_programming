8d530b08039214e81325ed3cac402e3fe3743cd5
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-10-26 06:39:57 -0700

    add nodeint

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * add_nodeint - add nodes to a list
+ *
+ * @head: node
+ * @n: int
+ *
+ * Return: node
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *first;
+
+	first = malloc(sizeof(listint_t));
+
+	if (first == NULL)
+		return (NULL);
+
+	first->n = n;
+
+	if (*head == NULL)
+		first->next = NULL;
+	else
+		first->next = *head;
+
+	*head = first;
+
+	return (*head);
+}
