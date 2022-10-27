66da72e1c95ac6c29feae7b98a68b75f601a6040
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-10-27 14:21:34 -0700

    binary to uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,4 @@
-#include "lists.h"
+#include "main.h"
 
 /**
  * _atoi - convert to int
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,5 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef MAIN_H
+#define MAIN_H
 
 unsigned int _atoi(char c);
 unsigned int binary_to_uint(const char *b);
