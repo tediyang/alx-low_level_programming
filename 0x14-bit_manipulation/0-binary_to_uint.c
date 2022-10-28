0093979a754c4c9c71ebb9b900a51c3780f3df0c
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-10-28 09:52:32 -0700

    bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * binary_to_uint = convert binary to decimal
+ *
+ * @b: string
+ *
+ * Return: value in decimal (base 10)
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	int i;
+
+	if (!b)
+		return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		result <<= 1;
+		if (b[i] == '1')
+			result += 1;
+
+	return (result);
+}
