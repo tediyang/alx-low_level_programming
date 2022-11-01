fda95dc7edc9fabc130e30a73d547d285914c88d
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2022-11-01 19:50:53 +0100

    made changes to files

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,24 +10,24 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int f, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (; text_content[len];)
+		for (len = 0; text_content[len];)
 			len++;
 	}
 
-	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
+	f = open(filename, O_WRONLY | O_APPEND);
+	w = write(f, text_content, len);
 
-	if (o == -1 || w == -1)
+	if (f == -1 || w == -1)
 		return (-1);
 
-	close(o);
+	close(f);
 
 	return (1);
 }
