9b4e6d67b31c462b6b2ef881f4d10187fa43f0b6
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-11-01 13:56:07 -0700

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,7 +11,7 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int f, w, len = 0;
+	int f, w, len;
 
 	if (filename == NULL)
 		return (-1);
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,11 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *create_buffer(char *file);
-void close_file(int fd);
-
 /**
  * create_buffer - Allocates 1024 bytes for a buffer.
+ *
  * @file: The name of the file buffer is storing chars for.
  *
  * Return: A pointer to the newly-allocated buffer.
@@ -20,8 +18,7 @@ char *create_buffer(char *file)
 
 	if (buffer == NULL)
 	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", file);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
 		exit(99);
 	}
 
@@ -30,6 +27,7 @@ char *create_buffer(char *file)
 
 /**
  * close_file - Closes file descriptors.
+ *
  * @fd: The file descriptor to be closed.
  */
 
@@ -48,6 +46,7 @@ void close_file(int fd)
 
 /**
  * main - Copies the contents of a file to another file.
+ *
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *
@@ -78,8 +77,7 @@ int main(int argc, char *argv[])
 	do {
 		if (from == -1 || r == -1)
 		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
 		}
@@ -87,8 +85,7 @@ int main(int argc, char *argv[])
 		w = write(to, buffer, r);
 		if (to == -1 || w == -1)
 		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			free(buffer);
 			exit(99);
 		}
