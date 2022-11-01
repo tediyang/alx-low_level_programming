cd645e5b9899b2ecb023661fe2f47fc4af6521aa
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2022-11-01 19:21:28 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,16 +11,15 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int f, w, len;
+	int f, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		len = 0;
 		while (text_content[len])
-			++len;
+			len++;
 	}
 
 	f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
