0b5d5ce93baa4658c8b308ce868461becad354a6
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2022-11-01 19:14:19 +0100

    change order of flagging

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 			++len;
 	}
 
-	f = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(f, text_content, len);
 
 	if (f == -1 || w == -1)
