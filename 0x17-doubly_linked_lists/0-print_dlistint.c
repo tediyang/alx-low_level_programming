dd4675e032cbde265031380fd1ed1da69f3881ee
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2022-12-13 10:27:52 +0100

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,20 +1,21 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: doubly linkedlist
+ * Return: number of nodes
+ */
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	if (h == NULL)
-		return (0);
-	
-	size_t count = 0;
+	size_t num_nodes = 0;
 
 	while (h)
 	{
-		printf("%d\n", h->n);
-		count++;
-	
+		printf("%i\n", h->n);
+		num_nodes++;
 		h = h->next;
 	}
-	
-	return count;
+
+	return (num_nodes);
 }
