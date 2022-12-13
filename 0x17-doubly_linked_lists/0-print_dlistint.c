bab874414af4c54e5fa7d4a96f36321b76886840
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2022-12-13 10:27:01 +0100

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,12 +1,12 @@
 #include "lists.h"
-#include <stdio.h>
+
 
 size_t print_dlistint(const dlistint_t *h)
 {
 	if (h == NULL)
 		return (0);
 	
-	size_t count = 0;z
+	size_t count = 0;
 
 	while (h)
 	{
