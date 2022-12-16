2f0bf5d0f3a573a61d2640665cf1803997115479
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2022-12-16 17:40:50 +0100

    Create 4-free_dlistint.c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - free dlistint.
+ *
+ * @head: the listnode
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = NULL;
+
+	if (head)
+	{
+		current = head;
+		while (current->next != NULL)
+			current = current->next;
+        while (current->prev != NULL)
+        {
+            current = current->prev;
+            free(current->next);
+        }
+	}
+
+	free(head);
+}
