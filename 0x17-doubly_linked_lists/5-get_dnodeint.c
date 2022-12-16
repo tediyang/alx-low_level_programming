5d13010837d6f5fbe532a5bf88a4cdc19b5936ce
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-12-16 14:44:17 -0800

    get_dnodeint

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - This gets the value of node a index.
+ *
+ * @head: the listnode
+ * @index: the index
+ *
+ * Return: the value of the index
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	size_t count = 0;
+
+	while (count < 5)
+	{
+		head = head->next;
+		count++;
+	}
+	return (head);
+}
