52a610bfcca92969303415d6f8dbfa468bfb15fe
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-12-16 15:13:41 -0800

    get_dnodeint

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	size_t count = 0;
 
-	while (count++ < index)
+	while (count++ < index && head)
 		head = head->next;
 	if (!head)
 		return (NULL);
