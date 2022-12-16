1d7781d30e4771e3faa75678502f31ccc6dcae0b
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2022-12-16 17:07:51 +0100

    Update 3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -11,18 +11,27 @@
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new = malloc(sizeof(dlistint_t));
+	dlistint_t *current = NULL, *new_node = NULL;
 
-	if (new == NULL)
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
 		return (NULL);
 
-	new->n = n;
-	new->next = NULL;
-	new->prev = *head;
-
+	new_node->n = n;
 	if (*head)
-		(*head)->next = new;
+	{
+		current = *head;
+		while (current->next != NULL)
+			current = current->next;
 
-	*head = new;
+		new_node->next = NULL;
+		new_node->prev = current;
+		current->next = new_node;
+		return (new_node);
+	}
+
+	new_node->next = *head;
+	new_node->prev = NULL;
+	*head = new_node;
 	return (*head);
 }
