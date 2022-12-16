8622051ee9f4db23de1cfc6d2608e54dd8ef743f
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-12-16 07:34:53 -0800

    add_dnodeint

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - return the new listnode.
+ *
+ * @head: pointer to a pointer parameter.
+ * @n: the int value
+ *
+ * Return: pointer to the list node.
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+
+	if (*head)
+		(*head)->prev = new;
+	*head = new;
+
+	return (*head);
+}
