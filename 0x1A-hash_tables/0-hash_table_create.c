5f75d38359ca540908259a77318794f3daf50ffa
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2023-01-11 22:07:59 +0100

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,17 +8,21 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned int i;
+	unsigned long int i;
 
 	hash_table_t *table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
-		return NULL;
+		return (NULL);
+
+	hash_node_t *array = calloc(table->size, sizeof(hash_node_t));
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		array[i] = NULL;
+
 	table->size = size;
-	table->array = calloc(table->size, sizeof(hash_node_t));
-	if (table->array == NULL)
-		return NULL;
-	for (i = 0; i < table->size; i++)
-		table->array[i] = NULL;
+	table->array = array;
 
 	return (table);
 }
