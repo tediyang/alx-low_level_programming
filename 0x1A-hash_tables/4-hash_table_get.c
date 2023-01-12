f1fbe231132d82895e931eb121f8d3a7fe7cc41e
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2023-01-12 02:39:00 +0100

    get the value of a key from the hash table

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - get the value of a key in the hash table.
+ * @ht: hash table.
+ * @key: The key.
+ * Return: The value.
+*/
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+    hash_node_t *item;
+    unsigned long int index;
+
+    if (ht == NULL || key[0] == '\0')
+        return (NULL);
+
+    index = key_index((const unsigned char *)key, ht->size);
+    item = ht->array[index];
+    if (item == NULL)
+        return (NULL);
+
+    return (item->value);
+}
