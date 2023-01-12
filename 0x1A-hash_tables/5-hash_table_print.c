2a9b83c68967f3a2e75f7683c8a7645f37cbd7b1
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2023-01-12 03:27:29 +0100

    Create 5-hash_table_print.c

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print the table.
+ * @ht: hash table.
+*/
+
+void hash_table_print(const hash_table_t *ht)
+{
+    unsigned long int i;
+    char *sep;
+
+    if (ht == NULL)
+        return;
+    
+    printf("{");
+    sep = "";
+    
+    for (i = 0; i < ht->size; i++)
+    {
+        if (ht->array[i])
+        {
+            printf("%s'%s': '%s'", sep, ht->array[i]->key, ht->array[i]->value);
+            sep = ", ";
+        }
+    }
+    printf("}\n");
+}
