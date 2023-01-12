284c21784d6d37c7c76d45e961cc4314b41c258e
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2023-01-12 02:04:10 +0100

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -7,24 +7,24 @@
  * Return: A node.
 */
 
-hash_node_t *create_node(char *key, char *value)
+hash_node_t *create_node(char *key, char* value)
 {
-    /* Creates a pointer to a new hash table node */
-    hash_node_t *node;
+        /* Creates a pointer to a new hash table node */
+        hash_node_t *node;
 
-    node = malloc(sizeof(hash_node_t));
-    if (node == NULL)
-        return (NULL);
-    node->key = malloc(strlen(key) + 1);
-    node->value = malloc(strlen(value) + 1);
-    if (node->key == NULL || node->value == NULL)
-        return (NULL);
+        node = malloc(sizeof(hash_node_t));
+        if (node == NULL)
+            return (NULL);
+        node->key = malloc(strlen(key) + 1);
+        node->value = malloc(strlen(value) + 1);
+        if (node->key == NULL || node->value == NULL)
+            return (NULL);
 
-    strcpy(node->key, key);
-    strcpy(node->value, value);
-    node->next = NULL;
+        strcpy(node->key, key);
+        strcpy(node->value, value);
+        node->next = NULL;
 
-    return node;
+        return node;
 }
 
 /**
@@ -37,38 +37,38 @@ hash_node_t *create_node(char *key, char *value)
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    hash_node_t *item;
-    hash_node_t *new;
-    unsigned long int index;
+        hash_node_t *item;
+        hash_node_t *new;
+        unsigned long int index;
 
-    if (ht == NULL || key[0] == '\0')
-    {
-        return (0);
-    }
-
-    index = key_index((const unsigned char *)key, ht->size);
-    item = ht->array[index];
-    new = create_node((char *)key, (char *)value);
-    if (new == NULL)
-        return (0);
-
-    if (item == NULL)
-    {
-        ht->array[index] = new;
-        return (1);
-    }
-    else
-    {
-        if (strcmp(item->key, key) == 0)
+        if (ht == NULL || key[0] == '\0')
         {
-            ht->array[index]->value = (char *)value;
+            return (0);
+        }
+
+        index = key_index((const unsigned char *)key, ht->size);
+        item = ht->array[index];
+        new = create_node((char *)key, (char *)value);
+        if (new == NULL)
+            return (0);
+
+        if (item == NULL)
+        {
+            ht->array[index] = new;
             return (1);
         }
         else
         {
-            new->next = item;
-            ht->array[index] = new;
-            return (1);
+            if (strcmp(item->key, key) == 0)
+            {
+                ht->array[index]->value = (char *)value;
+                return (1);
+            }
+            else
+            {
+                new->next = item;
+                ht->array[index] = new;
+                return (1);
+            }
         }
-    }
 }
