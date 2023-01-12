effb68196cb6c7e1c8d5d6f079eeef67d9b0dc81
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-01-11 17:43:18 -0800

    get key value

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,16 +9,16 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    hash_node_t *item;
-    unsigned long int index;
+	hash_node_t *item;
+	unsigned long int index;
 
-    if (ht == NULL || key[0] == '\0')
-        return (NULL);
+	if (ht == NULL || key[0] == '\0')
+		return (NULL);
 
-    index = key_index((const unsigned char *)key, ht->size);
-    item = ht->array[index];
-    if (item == NULL)
-        return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	item = ht->array[index];
+	if (item == NULL)
+		return (NULL);
 
-    return (item->value);
+	return (item->value);
 }
