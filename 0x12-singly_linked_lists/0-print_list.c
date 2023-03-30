4b84686189cce0e603ffe7f6ccf0c75f4df205ad
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-03-30 23:26:28 +0100

    0-print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,21 +11,27 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t num = 0;
 
+	/* Check if h doesn't contain any node */
 	if (h == NULL)
-		return (0);
+		return (num);
 
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+		/* check if str is present */
+		if (!h->str)
+			printf("[0] (nil)\n");
+		/* print the str value of node */
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
+		/* Then increment the num */
+		num++;
+
+		/* Move to the next node */
 		h = h->next;
-		count++;
 	}
 
-	return (count);
+	return (num);
 }
