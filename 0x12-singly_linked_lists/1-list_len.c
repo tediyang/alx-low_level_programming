2538a9386cc8ff2baf6f225443383d396ce51e06
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-04-08 00:47:20 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,25 @@
+#include <studio.h>
+
+/**
+ * list_len - print the number of list node.
+ *
+ * @h: param
+ *
+ * Return: the number of list node.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t num = 0;
+
+	if (!h)
+		return (0);
+
+	while (h)
+	{
+		h = h->next;
+		num++;
+	}
+
+	return (num);
+}
