25ee51b53f9e4d004529094df2ed840acafa64c5
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-04-08 04:20:45 -0700

    3-add_node_end.c

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+ * add_node_end - add to the end of the list.
+ *
+ * @head: listnode.
+ * @str: string.
+ *
+ * Return: listnode
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t *c_head;
+	char *dup;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+
+	dup = strdup(str);
+	if (!dup)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->str = dup;
+	new->len = strlen(str);
+	new->next = NULL;
+
+	c_head = *head;
+
+	if (c_head)
+	{
+		c_head = new;
+		return (*head);
+	}
+
+	while (c_head->next)
+		c_head = c_head->next;
+	c_head->next = new;
+	return (*head);
+}
