0549bf76981449e19d0f17d5f61c157c68249ae7
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-04-08 01:32:28 -0700

    2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * add_node - add node.
+ *
+ * @head: the listnode.
+ * @str: the string literal.
+ *
+ * Return: the number of list node.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+
+	new->str = str;
+	new->len = strlen(str);
+	new->next = NULL;
+
+	if (!*head)
+	{
+		*head = new;
+		return (*head);
+	}
+
+	while (*head)
+	{
+		new->next = *head;
+		*head = new;
+	}
+	return (*head);
+}
